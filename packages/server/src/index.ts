38b7b3cbde5b297f509c2c548d038f3148d9752d
Alex Sandiiarov <sandiyarov@gmail.com>
2018-06-27 20:22:55 -0500

    upd

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,101 +1,17 @@
-import { GraphQLServer } from 'graphql-yoga';
-import { Prisma } from 'prisma-binding';
-
-const resolvers = {
-  Query: {
-    posts: (_: any, args: any, context: any, info: any) => {
-      return context.prisma.query.posts(
-        {
-          where: {
-            OR: [
-              { title_contains: args.searchString },
-              { content_contains: args.searchString },
-            ],
-          },
-        },
-        info
-      );
-    },
-    user: (_: any, args: any, context: any, info: any) => {
-      console.log(info);
-      return context.prisma.query.user(
-        {
-          where: {
-            id: args.id,
-          },
-        },
-        info
-      );
-    },
-  },
-  Mutation: {
-    createDraft: (_: any, args: any, context: any, info: any) => {
-      return context.prisma.mutation.createPost(
-        {
-          data: {
-            title: args.title,
-            content: args.title,
-            author: {
-              connect: {
-                id: args.authorId,
-              },
-            },
-          },
-        },
-        info
-      );
-    },
-    publish: (_: any, args: any, context: any, info: any) => {
-      return context.prisma.mutation.updatePost(
-        {
-          where: {
-            id: args.id,
-          },
-          data: {
-            published: true,
-          },
-        },
-        info
-      );
-    },
-    deletePost: (_: any, args: any, context: any, info: any) => {
-      return context.prisma.mutation.deletePost(
-        {
-          where: {
-            id: args.id,
-          },
-        },
-        info
-      );
-    },
-    signup: (_: any, args: any, context: any, info: any) => {
-      return context.prisma.mutation.createUser(
-        {
-          data: {
-            name: args.name,
-          },
-        },
-        info
-      );
-    },
-  },
-};
-
-const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466',
-  secret: process.env.PRISMA_SECRET,
-  debug: true,
-});
+import { GraphQLServer } from 'graphql-yoga'
+import { Prisma } from './generated/prisma'
+import resolvers from './resolvers'
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: './src/schema.graphql',
   resolvers,
-  resolverValidationOptions: {
-    requireResolversForResolveType: false,
-  },
-  context: req => ({ ...req, prisma }),
-});
-server.start(() =>
-  console.log(`GraphQL server is running on http://localhost:4000`)
-);
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
+    }),
+  }),
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
--- packages/server/src/resolvers/index.ts
+++ packages/server/src/resolvers/index.ts
@@ -1,26 +1,15 @@
-// import { extractFragmentReplacements } from "prisma-binding";
-// import { Query } from "./Query";
-// import { Subscription } from "./Subscription";
-// import { auth } from "./Mutation/auth";
-// import { Home } from "./Home";
-// import { ExperiencesByCity } from "./ExperiencesByCity";
-// import { Viewer } from "./Viewer";
-// import { AuthPayload } from "./AuthPayload";
-// import { book } from "./Mutation/book";
-// import { addPaymentMethod } from "./Mutation/addPaymentMethod";
+import { Query } from './Query'
+import { Subscription } from './Subscription'
+import { auth } from './Mutation/auth'
+import { post } from './Mutation/post'
+import { AuthPayload } from './AuthPayload'
 
-// export const resolvers = {
-//   Query,
-//   Mutation: {
-//     ...auth,
-//     book,
-//     addPaymentMethod
-//   },
-//   Subscription,
-//   Viewer,
-//   ExperiencesByCity,
-//   Home,
-//   AuthPayload
-// };
-
-// export const fragmentReplacements = extractFragmentReplacements(resolvers);
+export default {
+  Query,
+  Mutation: {
+    ...auth,
+    ...post,
+  },
+  Subscription,
+  AuthPayload,
+}
